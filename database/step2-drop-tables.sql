b4ac76f38ef18c32b421598d4552e110fb63cd91
树宝 <2054088516@qq.com>
2025-10-19 20:34:46 +0800

    T2

---
+++ database/step2-drop-tables.sql
@@ -0,0 +1,5 @@
+-- 步骤2：删除现有表
+DROP TABLE IF EXISTS user_profiles CASCADE;
+DROP TABLE IF EXISTS historical_figures CASCADE;
+DROP TABLE IF EXISTS cultural_heritage CASCADE;
+DROP TABLE IF EXISTS charity_projects CASCADE;
